c00c6b6d0b50e1d6195e9038872801dcdc55c5d0
work <work@Akashs-MacBook-Air-2.local>
2025-03-06 17:47:40 +0530

    ijwn

--- frontend/src/api/HospitalAPI.js
+++ frontend/src/api/HospitalAPI.js
@@ -1,4 +1,4 @@
-const API_URL = "https://hospitalmanagementsystem-1-sh3i.onrender.com";
+const API_URL = `https://hospitalmanagementsystem-1-sh3i.onrender.com/api/hospitals`;
 
 // Get all hospitals
 export const getAllHospitals = async () => {
